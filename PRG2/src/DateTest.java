c95e3d7bd2f56bbd6c4326b29c5e468291997034
smr <smr@SPCCLT52.spc.local>
2019-05-07 18:39:25 +0200

    new file date

---
+++ PRG2/src/DateTest.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class DateTest {
+	public static void main(String[] args) {
+		Date date = new Date(0);
+		System.out.println(date);
+		System.out.println(date.getTime());
+	}
+
+}
